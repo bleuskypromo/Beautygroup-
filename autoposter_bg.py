bc7a0c6d52d84c2d053e8a368f5870baa5141a09
bleuskypromo <bleuskypromo@gmail.com>
2025-11-28 23:22:20 +0100

    Update autoposter_bg.py

--- autoposter_bg.py
+++ autoposter_bg.py
@@ -10,7 +10,7 @@ FEED_URI = "at://did:plc:jaka644beit3x4vmmg6yysw7/app.bsky.feed.generator/aaamyq
 
 MAX_PER_RUN = 100          # max reposts per run
 MAX_PER_USER = 3           # max reposts per account per run
-HOURS_BACK = 12            # tijdvenster: laatste 3 uur
+HOURS_BACK = 3            # tijdvenster: laatste 3 uur
 SLEEP_SECONDS = 2          # vertraging tussen reposts/likes
 REPOST_LOG = "reposted_bg.txt"  # eigen logbestand voor BeautyGroup
 
